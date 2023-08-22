eb4310333a785842f972571d1ae2798813e02285
Munjusjnr <munjusjnr54@outlook.com>
2023-08-22 04:58:41 -0700

    update with sql setup and testing

---
+++ setup_mysql_test.sql
@@ -0,0 +1,11 @@
+-- Prepares a MySQL server for the project
+-- Database hbnb_test_db
+-- new user hbnb_test (in localhost)
+-- User will have password of hbnb_dev should be set to hbnb_test_pwd
+-- hbnb_test should have all privileges on the database hbnb_test_db
+-- hbnb_test should have SELECT privilege on the database performance_schema
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
