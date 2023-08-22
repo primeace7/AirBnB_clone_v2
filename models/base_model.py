0944084ab05266610a55e47921a050271791fae5
Munjusjnr <munjusjnr54@outlook.com>
2023-08-22 08:49:53 -0700

    update on the basemodel file

--- models/base_model.py
+++ models/base_model.py
@@ -2,25 +2,32 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+import models
 
 
-class BaseModel:
+class BaseModel():
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+        """This method initantializes three public instance attributes.
+
+        Args:
+            *arg(tuple): This is a tuple that contains parameters
+            **kwargs(dict): This is a dictionary that contains parameterd and
+            their values.
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        if kwargs:
+            if '__class__' in kwargs:
+                kwargs.pop('__class__')
             self.__dict__.update(kwargs)
+            try:
+                self.created_at = datetime.fromisoformat(self.created_at)
+                self.updated_at = datetime.fromisoformat(self.updated_at)
+            except Exception as e:
+                pass
+
 
     def __str__(self):
         """Returns a string representation of the instance"""
