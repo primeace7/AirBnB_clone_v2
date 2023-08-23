db9c1fd226d749acffd2a04ae652765f1aea28f5
Munjusjnr <munjusjnr54@outlook.com>
2023-08-23 10:51:26 -0700

    amenities file updated

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import String
+from sqlalchemy import Column
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ The amenity class, represents the amenities section
+    of the mysqldatabase
+
+    Inherits from both BaseModel and Base(sqlalchemy)
+
+    Attributes:
+        __tablename__: represents the table name, amenities
+        name: represents a column containing a string
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
--- models/place.py
+++ models/place.py
@@ -5,6 +5,19 @@ from models.base_model import Base
 from sqlalchemy import Column, String, Integer, ForeignKey, Float
 from sqlalchemy.orm import relationship
 from os import getenv
+import models
+from models.amenity import Amenity
+from models.review import Review
+from sqlalchemy import Table
+
+
+association_table = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -34,6 +47,9 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     reviews = relationship("Review", backref="place", cascade="delete")
+    amenities = relationship("Amenity", secondary="place_amenity",
+                            viewonly=False)
+    amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
@@ -46,3 +62,22 @@ class Place(BaseModel, Base):
                 if review.place.id == self.id:
                     list_of_reviews.append(review)
             return list_of_reviews
+
+        @property
+        def amenities(self):
+            """Getter attribute reviews that returns the list of amenities
+            a place has
+            """
+            list_of_amenities = []
+            for amenity in models.storage.all(Amenity).values():
+                if amenity.id in self.amenity_ids:
+                    list_of_amenities.append(amenity)
+            return list_of_amenities
+
+        @amenities.setter
+        def amenities(self, value):
+            """Setter attribute amenities that handles append method for adding
+            an Amenity.id to the attribute amenity_ids.
+            """
+            if type(value) == Amenity:
+                self.amenity_ids.append(value.id)
