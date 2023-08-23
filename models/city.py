d050ba37c68b534a4e45e3291ef05f419b6742f0
primeace7 <osaruonamenjeffery@gmail.com>
2023-08-23 11:55:20 +0000

    Add task 8 code modifications

--- models/city.py
+++ models/city.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy.orm import relationship
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
     state_id = ""
     name = ""
+
+    places = relationship('Place', back_populates='places')
