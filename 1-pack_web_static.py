868b1d94f2982e82c06716d982210b634741eab2
primeace7 <osaruonamenjeffery@gmail.com>
2023-09-09 15:24:25 +0000

    Add task 1 deploy static file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+'''
+Generate an archive of files
+'''
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    '''
+    Compress web static files into an archive
+    '''
+
+    # get time of archive creation
+    creation_time = datetime.now().strftime('%Y%m%d%H%M%S')
+
+    local('mkdir version')
+
+    path = "version/web_static_{}.tgz".format(creation_time)
+
+    # create the backup file
+    local('touch {}'.format(path))
+
+    command = "tar -cvzf {} web_static".format(path)
+
+    if local(command).succeeded:
+        return path
+    else:
+        return None
