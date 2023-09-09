74ffac443acd69f0120efc8aa6ad15e43f2bb834
primeace7 <osaruonamenjeffery@gmail.com>
2023-09-09 15:49:26 +0000

    Refactor task 1 deployment file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -16,9 +16,9 @@ def do_pack():
     # get time of archive creation
     creation_time = datetime.now().strftime('%Y%m%d%H%M%S')
 
-    local('mkdir -p version')
+    local('mkdir -p versions')
 
-    path = "version/web_static_{}.tgz".format(creation_time)
+    path = "versions/web_static_{}.tgz".format(creation_time)
 
     # create the backup file
     local('touch {}'.format(path))
